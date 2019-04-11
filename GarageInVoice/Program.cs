f120a67bec5e72409cc7f68b0128d3d926092c4e
Shane Coppinger <S00190494@mail.itsligo.ie>
2019-04-11 19:42:15 +0100

    Add project files.

---
+++ GarageInVoice/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageInVoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string theName,
+                   theReg;
+            int theMileage,
+                theLabour,
+                theCost,
+                quit;
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //Ask user for customer info
+
+            theName = GetCustomerInfo("the customer name: ");
+            theReg = GetCustomerInfo("the registration of the customer's car: ");
+
+            theMileage = GetCarMileage("the mileage of the car: ");
+            theLabour = GetJobCosts("the number of hours worked: ");
+            theCost = GetJobCosts("the cost of the parts for this job: ");
+            Console.Clear();
+
+            //Create the firsst invoice object
+            ServiceInvoice s1 = new ServiceInvoice(theName, theReg, theMileage, theLabour, theCost);
+            Console.WriteLine("PETE'S REPAIR SHOP");
+            Console.WriteLine();
+            Console.WriteLine("Customer invoice");
+            Console.WriteLine();
+            
+            Console.WriteLine(s1.ToString());
+
+            //Console.WriteLine();
+            //Console.WriteLine("    -1 to quit");
+            //quit = Convert.ToInt32(Console.ReadLine());
+
+            //Loop to create more invoice objects
+            string entry;
+            int selection = 0;
+            Console.WriteLine("Please press enter to continue and -1 to quit");
+            entry = Console.ReadLine();
+            bool validEntry = int.TryParse(entry, out selection);
+            if (validEntry == false)
+            {
+                Console.WriteLine("Invalid Data entered ");
+            }
+
+            
+            while (selection != -1)
+            {
+                Console.Clear();
+                
+                theName = GetCustomerInfo("the customer name: ");
+                theReg = GetCustomerInfo("the registration of the customer's car: ");
+
+                theMileage = GetCarMileage("the mileage of the car: ");
+                theLabour = GetJobCosts("the number of hours worked: ");
+                theCost = GetJobCosts("the cost of the parts for this job: ");
+                Console.Clear();
+
+                ServiceInvoice newInvoice = new ServiceInvoice(theName, theReg, theMileage, theLabour, theCost);
+                Console.WriteLine("PETE'S REPAIR SHOP");
+                Console.WriteLine();
+                Console.WriteLine("Customer invoice");
+                Console.WriteLine();
+                Console.WriteLine(newInvoice.ToString());
+                Console.ReadLine();
+            }
+
+            
+        }
+
+        //Get customer info
+        public static string GetCustomerInfo(string input)
+        {
+            Console.Write("Please enter {0}", input);
+            return (Console.ReadLine());
+        }
+
+        //Get mileage
+        public static int GetCarMileage(string input)
+        {
+            string miles;
+            int theMiles = 0;
+            Console.Write("Please enter {0}", input);
+            miles = Console.ReadLine();
+            bool validEntry = int.TryParse(miles, out theMiles);
+            if (validEntry == false )
+            {
+                Console.WriteLine("Invalid Data entered - 0 will be entered");
+            }
+
+            return theMiles;
+
+
+            //miles = Console.ReadLine();
+            //if (int.TryParse(Console.ReadLine(), out theMiles) == false)
+
+            //{
+            //    Console.WriteLine("Invalid Data entered");
+            //}
+            //return theMiles;
+        }
+
+        //Get Job costs
+        public static int GetJobCosts (string input)
+        {
+            string userInput;
+            int UserNum;
+            Console.Write("Please enter {0}", input);
+
+            userInput = Console.ReadLine();
+            bool validEntry = int.TryParse(userInput, out UserNum);
+            if (validEntry == false)
+            {
+                Console.WriteLine("Invalid Data entered - 0 will be entered");
+            }
+            return UserNum;
+
+            //return theMiles;
+
+            //userInput = Console.ReadLine();
+            //if (int.TryParse(Console.ReadLine(), out UserNum) == false)
+
+            //{
+            //    Console.WriteLine("Invalid Data entered");
+            //}
+            //return UserNum;
+        }
+
+
+    }
+}
---
+++ GarageInVoice/ServiceInvoice.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageInVoice
+{
+    class ServiceInvoice
+    {
+        //constants
+        private const int LABOUR_CHARGE = 45;
+        private const decimal PARTS_VAT = .135m;
+        private const decimal LABOUR_VAT = .215M;
+        private const decimal DISCOUNT_APPLIED = .05M;
+
+        #region properties
+        public string Name { get; set; }
+        public string Registration { get; set; }
+        public int Odometer { get; set; }
+        public int Labour { get; set; }
+        public int CostofParts { get; set; }
+        #endregion properties
+
+        #region Constructors
+        public ServiceInvoice()
+        {
+
+        }
+
+        public ServiceInvoice(string aName, string aReg, int mileage, int workDone, int parts)
+        {
+            Name = aName;
+            Registration = aReg;
+            Odometer = mileage;
+            Labour = workDone;
+            CostofParts = parts;
+        }
+
+        public ServiceInvoice(string aName, string aReg)
+        {
+            Name = aName;
+            Registration = aReg;
+            Odometer = 0;
+            Labour = 0;
+            CostofParts = 0;
+        }
+        #endregion Constructors
+
+        //Get charge for labour
+        public int GetLabourCharge()
+        {
+            return Labour * LABOUR_CHARGE;
+        }
+
+        //Get VAT for parts
+        public decimal GetVatOfParts()
+        {
+            return CostofParts * PARTS_VAT;
+        }
+
+        //Get VAT for labour
+        public decimal GetVatOfLabour()
+        {
+            return GetLabourCharge() * LABOUR_VAT;
+        }
+
+        //Get Gross
+        public decimal GetGross()
+        {
+            return GetLabourCharge() + CostofParts;
+        }
+
+        //Get subtotal
+        public decimal GetSubTotal()
+        {
+           return GetLabourCharge() + GetVatOfLabour() + CostofParts + GetVatOfParts();
+        }
+
+        //Check for discount
+        public decimal GetDiscount()
+        {
+            decimal discount = 0;
+            if (GetSubTotal()> 100)
+            {
+                //decimal discount =0;
+                discount = GetSubTotal() * DISCOUNT_APPLIED;
+                
+            }
+            return discount;
+        }
+
+        //Get total
+        public decimal GetTotal()
+        {
+            return GetSubTotal() - GetDiscount();
+        }
+
+        //Output string representation
+        public override string ToString()
+        {
+            return string.Format ("Date: 24/11/18" +
+                "\nName: {0}" +
+                "\nVehicle registration: {1}"  +
+                "\nMileage: {2}" +
+                "\n\n_______________________________" +
+                "\nDescription               Cost" +
+                "\n----------            -----------" +
+                "\nLabour: {3:C}" +
+                "\nParts: {4:C}" +
+                "\n" +
+                "\nGross: {5:C}" +
+                "\nVAT on Parts: {6:C}" +
+                "\nVAT on Labour: {7:C}" +
+                "\n" +
+                "\nsub total: {8:C}" +
+                "\nDiscount: {9:C}" +
+                "\n" +
+                "Total: {10:C}", Name, Registration, Odometer, GetLabourCharge(), CostofParts, GetGross(), GetVatOfParts(), +
+                GetVatOfLabour(), GetSubTotal(), GetDiscount(), GetTotal() );
+        }
+    }
+}
